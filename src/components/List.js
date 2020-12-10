068d2a08eab81b2b40b6e5fc980c71ff8aa3d5b4
Jenny <finej01@gmail.com>
2020-12-10 12:36:23 -0500

    update

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,8 @@
 function List(props) {
-
-    const fruitItems = props.fruits.map((fruit,i) => <li>{fruit}</li>)
+  const fruitItems = props.fruits.map((fruit, i) => <li>{fruit}</li>);
   return (
     <div>
-        <ul>
-
-            {fruitItems}
-        </ul>
-
+      <ul>{fruitItems}</ul>
     </div>
   );
 }
